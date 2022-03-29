5e04ec34af302e9c3f36201ddc61a4807f5b5861
DESKTOP-DCH2DUT\小黎子！！！ <2019136591@qq.com>
2022-03-29 11:37:21 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,43 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
+import Login from '../views/LoginView.vue'
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+const routes = [{
+        path: "/",
+        name: "home",
+        component: HomeView,
+        children: [{
+                path: "",
+                meta: ["首页"],
+                name: 'HomeIndex',
+                component: () =>
+                    import ("../components/HomeIndex.vue")
+            }, {
+                path: "2-1",
+                meta: ["文章列表"],
+                component: () =>
+                    import ("../components/HomeWenz.vue")
+            },
+            {
+                path: "2-2",
+                meta: ["多层级路由示列"],
+                component: () =>
+                    import ("../components/HomeRouter.vue")
+            },
+
+        ]
+    },
+    {
+        path: "/login",
+        name: "MyLogin",
+        component: Login
+    },
 ];
 
 const router = new VueRouter({
-  routes,
+    routes,
 });
 
 export default router;
